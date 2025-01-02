a6516862a98d973794158194ef6a9304c098b991
Dekarontohos <burjui2001@gmail.com>
2025-01-02 17:04:24 +0300

    configure the project

--- src/App.js
+++ src/App.js
@@ -25,32 +25,13 @@
 
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
 
 export const App = () => {
-	return React.createElement(
-		"div",
-		{ className: "App" },
-		React.createElement(
-			"header",
-			{ className: "App-header" },
-			React.createElement(
-				"p",
-				null,
-				"Edit ",
-				React.createElement("code", null, "src/App.js"),
-				" and save to reload.",
-			),
-			React.createElement(
-				"a",
-				{
-					className: "App-link",
-					href: "https://reactjs.org",
-					target: "_blank",
-					rel: "noopener noreferrer",
-				},
-				"Learn React",
-			),
-			React.createElement("p", null, new Date().getFullYear()),
-		),
+	return (
+		<div>
+			<i className="fa fa-camera-retro"></i>
+			123
+		</div>
 	);
 };
