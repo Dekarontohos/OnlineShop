3a6f9da261e292c25cdaaa42bf4b47a420e913d4
Dekarontohos <burjui2001@gmail.com>
2025-01-05 12:07:01 +0300

    create db

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// export const App = () => {                //декларативный, императивного вроде бы нет
-// 	return (
-// 		<div className="App">
-// 			<header className="App-header">
-// 				<img src={logo} className="App-logo" alt="logo" />
-// 				<p>
-// 					Edit <code>src/App.js</code> and save to reload.
-// 				</p>
-// 				<a
-// 					className="App-link"
-// 					href="https://reactjs.org"
-// 					target="_blank"
-// 					rel="noopener noreferrer"
-// 				>
-// 					Learn React
-// 				</a>
-// 				<p>{new Date().getFullYear()}</p>
-// 			</header>
-// 		</div>
-// 	);
-// };
-
 import React from "react";
 import "./App.css";
 import styled from "styled-components";
+
+const Div = styled.div`
+	text-align: center;
+`;
 
 export const App = () => {
 	return (
 		<div>
 			<i className="fa fa-camera-retro"></i>
 			123
+			<Div>123</Div>
 		</div>
 	);
 };
